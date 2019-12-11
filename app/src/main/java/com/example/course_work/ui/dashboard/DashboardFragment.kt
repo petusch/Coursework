85eb03774015e2eb03c05ddc40caed941f72422c
Oleg <34604253+petusch@users.noreply.github.com>
2019-12-11 04:34:56 +0300

    Login Attempt

--- app/src/main/java/com/example/course_work/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/course_work/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.example.course_work.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,8 @@ import androidx.lifecycle.Observer
 
 import androidx.lifecycle.ViewModelProviders
 import com.example.course_work.R
+import com.example.course_work.ui.CommonActivity
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
 
@@ -33,4 +36,10 @@ class DashboardFragment : Fragment() {
         return root
     }
 
-}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text_notifications1.setOnClickListener{
+            val intent = Intent(context, CommonActivity::class.java)
+            // start your next activity
+            startActivity(intent)    }
+}}
--- app/src/main/java/com/example/course_work/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/course_work/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.course_work.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,23 +10,29 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.course_work.R
+import com.example.course_work.ui.CommonActivity
+import com.example.course_work.ui.LogIn.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
         return root
     }
-}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        LogInbutton.setOnClickListener{
+            val intent = Intent(context, LoginActivity::class.java)
+            // start your next activity
+            startActivity(intent)    }
+        }
+    }
